eeaf4916876dd1ea340fd573f6aca1c9c040094d
vbystroem <veronica.bystroem@gmail.com>
2025-11-10 17:10:33 +0100

    Added some styling and figured out container queries

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,73 +1,88 @@
 <script>
-    import { getRecipes } from "../MockApiData.js";
+import { getRecipes } from "../MockApiData.js";
 
-    export default {
-        data() {
-            return {
-                recipes: getRecipes(),
-            }
-        },
-        props: {
-            title: String,
-            description: String,
-            rating: Number,
-            ingredientCount: Number,
-            cookTimeMinutes: Number,
-        },
-        
-    }
+export default {
+  data() {
+    return {
+      recipes: getRecipes(),
+    };
+  },
+  props: {
+    title: String,
+    description: String,
+    rating: Number,
+    ingredientCount: Number,
+    cookTimeMinutes: Number,
+  },
+};
 </script>
 
 <template>
+  <div class="card-wrapper">
     <div class="card-container">
-        <div class="card-imagewrapper">
-            <img src="../assets/images/placeholder-image.jpg" :alt="title">
+      <div class="card-imagewrapper">
+        <img src="../assets/images/placeholder-image.jpg" :alt="title" />
+      </div>
+      <div class="card-content">
+        <h2>{{ title }}</h2>
+        <p>{{ description }}</p>
+        <RatingDisplay>Test</RatingDisplay>
+        <div class="recipe-overview-short">
+          <p>{{ ingredientCount }} ingredients</p>
+          <p>{{ cookTimeMinutes }} minutes</p>
         </div>
-        <div class="card-content">
-            <h2>{{ title }}</h2>
-            <p>{{ description }}</p>
-            <RatingDisplay>Test</RatingDisplay>
-            <div class="recipe-overview-short">
-                <p>{{ ingredientCount }} ingredients</p>
-                <p>{{ cookTimeMinutes }} minutes</p>
-            </div>
-        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
-    .card-container {
-        container-type: inline-size;
-        container-name: recipe-card;
-        max-width: calc(100vw / 3);
-        display: flex;
-        flex-direction: column;
-        justify-content: start;
-        align-items: start;
-        background-color: var(--color-secondary-light);
-    }
+.card-wrapper {
+  container-type: inline-size;
+  container-name: recipe-card;
+}
+.card-container {
+  width: min(93.5vw, 650px);
+  margin: 0.5rem;
+  border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  background-color: var(--color-secondary-light);
+}
 
-    .card-imagewrapper{
-        width: 100px;
-    }
+.card-imagewrapper,
+.card-imagewrapper > img {
+  width: 300px;
+  height: 300px;
+  
+}
 
-    .card-imagewrapper>img {
-        width: 100%;
-        height: auto;
-    }
+.card-imagewrapper>img {
+    object-fit: cover;    
+}
+
+.card-content {
+  display: flex;
+  flex-direction: column;
+  padding-left: 2rem;
+}
+
+.recipe-overview-short {
+  display: flex;
+}
+
+@container recipe-card (min-width: 500px) {
+  .card-container {
+    flex-direction: row;
+    align-items: center;
     
-    .card-content {
-        display: flex;
-        flex-direction: column;
-    }
+  }
 
-    .recipe-overview-short {
-        display: flex;
-    }
-
-    @container recipe-card (width > 500px) {
-        .card-container {
-            flex-direction: row;
-        }
-    }
+  .card-imagewrapper>img {
+    border-top-left-radius: 0.5rem;
+  border-bottom-left-radius: 0.5rem;
+  }
+}
 </style>
