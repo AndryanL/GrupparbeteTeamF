2d624464955850bf7be73868d83e6e709e5bd7f4
viktoria.khrypun2002@gmail.com <viktoria.khrypun2002@gmail.com>
2025-11-11 10:26:23 +0100

    Merge branch 'dev' of https://github.com/AndryanL/GrupparbeteTeamF into dev
    Co-authored-by: mariagileborn-pixel <mariagileborn-pixel@users.noreply.github.com>
    Co-authored-by: Veronica Byström <vbystroem@users.noreply.github.com>
    Co-authored-by: AndryanL <andryan.lenander@chasacademy.se>

---
+++ src/components/Button.vue
@@ -0,0 +1,36 @@
+<template>
+    <button class="knapp" v-on:click = "handleClick">Knapp</button>
+</template>
+
+<script>
+    export default {
+        methods: {
+            handleClick() {
+                alert('clicked')
+            }
+        }
+}
+
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Playwrite+DK+Uloopet:wght@100..400&display=swap');
+</style>
+
+<style>
+    .knapp{
+        cursor: pointer;
+        min-width: 181px;
+        padding: 14px 52px;
+        border-radius: 5px;
+        border: none;
+        background: #FF909F;
+        color: #FFFAF6;
+        font-family: "Playwrite DK Uloopet", cursive;
+        font-size: 24px;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+    }
+</style>
---
+++ src/components/ReceptOverview.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="receptWrapper">
+        <div class="receptOverview" v-for="recipe in recipes" :key="recipe.id">
+            <p class="receptInfo">
+                <span data-ingredient-count>{{recipe.ingredientCount}}</span>&nbsp;<span>Ingredienser / </span>
+                <span data-time-count>{{recipe.time}}</span>&nbsp;<span>minuter</span>
+            </p>
+            <ul class="receptRating">
+                <li class="rating" v-for="n in recipe.rating" :key="n"></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .receptOverview{
+        width: 400px;
+        display: flex;
+        align-items: center;
+        
+        border-radius: 10px;
+        background: #381C10;
+        color: #FFC2CA;
+
+        font-family: "Playwrite DK Uloopet", cursive;
+        font-size: 16px;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+        
+    }
+
+    .receptInfo{
+        padding: 0 20px;
+        white-space: nowrap;
+    }
+
+    .receptRating{
+        list-style: none;
+        display: flex;
+        align-items: center;
+        gap: 5px;
+
+        padding: 20px 25px;
+        
+        background: #FFC2CA;
+        border-radius: 0 10px 10px 0;
+    }
+
+    .rating{
+        width: 16px;
+        height: 12px;
+        background: url(/src/assets/rating.svg);
+    }
+    
+    .rating:last-child{
+        background: url(/src/assets/rating-half.svg);
+    }
+
+</style>
+
+<script setup>
+import { ref } from 'vue'
+
+const recipes = ref([{ingredientCount: 5, time: 15, rating: 5, id: 1, name: "blabla"}]);
+
+// const recipe = ref({ingredientCount: 5, time: 15, rating: 5, id: 1, "name": "blabla"});
+
+</script>
