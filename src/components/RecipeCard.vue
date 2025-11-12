a16ef53a69975b7ff8527715dac768101639cecf
vbystroem <veronica.bystroem@gmail.com>
2025-11-12 15:48:51 +0100

    Fine-tuned the recipe card, so that it scales smoothly

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -24,7 +24,7 @@ export default {
         <img src="../assets/images/placeholder-image.jpg" :alt="title" />
       </div>
       <div class="card-content">
-        <h2>{{ title }}</h2>
+        <h2 class="card-h2">{{ title }}</h2>
         <p>{{ description }}</p>
         <div class="recipe-details">
           <RatingDisplay>Test</RatingDisplay>
@@ -50,8 +50,6 @@ export default {
   border-radius: 0.5rem;
   display: flex;
   flex-direction: column;
-  justify-content: start;
-  align-items: start;
   background-color: var(--color-secondary-mid);
   box-shadow: 0 0 0.2rem rgba(0, 0, 0, 0.5);
 }
@@ -59,10 +57,10 @@ export default {
 .card-imagewrapper,
 .card-imagewrapper > img {
   width: 100%;
-  height: 300px;
 }
 
 .card-imagewrapper > img {
+  height: 100%;
   object-fit: cover;
   border-top-right-radius: 0.5rem;
   border-bottom-right-radius: 0rem;
@@ -76,47 +74,52 @@ export default {
   justify-content: space-between;
   padding: 1.5rem 2rem;
   color: var(--color-primary-dark);
-  height: 300px;
+}
+
+.card-content > * {
+  padding-bottom: 1rem;
+}
+
+.card-content:last-child {
+  padding-bottom: 0;
 }
 
 .card-content p {
   font-family: "Comfortaa", sans-serif;
-  font-size: 1.1rem;
 }
 
 .recipe-overview-short {
   display: flex;
 }
 
+.recipe-overview-short > p {
+  font-family: "Playwrite DK Uloopet", cursive;
+}
+
 .recipe-overview-short > p:first-child {
   margin-right: 0.5rem;
 }
 
-h2 {
+.card-h2 {
   font-family: "Playwrite DK Uloopet", cursive;
-  font-size: 1.75rem;
-  hyphens: auto;
+  font-size: 1.25rem;
+  hyphens: manual;
   line-height: 2.5ex;
-  margin-bottom: 1rem;
+  padding-bottom: 1.25rem;
 }
 
-@container recipe-card (min-width: 625px) {
+@container recipe-card (min-width: 600px) {
   .card-container {
     flex-direction: row;
-    align-items: center;
   }
 
   .card-imagewrapper,
   .card-imagewrapper > img {
-    width: 300px;
+    width: 250px;
     border-top-right-radius: 0rem;
     border-bottom-right-radius: 0rem;
     border-top-left-radius: 0.5rem;
     border-bottom-left-radius: 0.5rem;
-  }
-
-  .card-content {
-    align-items: space-between;
   }
 }
 </style>
