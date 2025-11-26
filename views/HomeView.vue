51fdb28e418dbec692013506b1af35fcebdcfa22
vbystroem <veronica.bystroem@gmail.com>
2025-11-26 10:34:56 +0100

    Created functionality for dynamic routing using params

--- views/HomeView.vue
+++ views/HomeView.vue
@@ -1,3 +1,29 @@
+<script>
+import { getRecipes } from "../src/MockApiData.js";
+import RecipeCard from "../src/components/RecipeCard.vue";
+
+export default {
+  data() {
+    return {
+      recipes: getRecipes(),
+    };
+  },
+  components: {
+    RecipeCard,
+  },
+};
+</script>
+
 <template>
-    test home
+  <h1>HOME VIEW</h1>
+  <div v-for="recipe in recipes" :key="recipe.id">
+    <RecipeCard
+      @click="$router.push({ name: 'recipe', params: { id: recipe.id } })"
+      :id="recipe.id"
+      :title="recipe.title"
+      :description="recipe.description"
+      :ingredientCount="recipe.ingredients.length"
+      :cookTimeMinutes="recipe.cookTimeMinutes"
+      :rating="recipe.rating"></RecipeCard>
+  </div>
 </template>
--- views/RecipeView.vue
+++ views/RecipeView.vue
@@ -1,3 +1,48 @@
+<script>
+import { getRecipes } from "../src/MockApiData.js";
+import RecipeCard from "../src/components/RecipeCard.vue";
+import StepList from "../src/components/StepList.vue";
+
+export default {
+  data() {
+    return {
+      recipes: getRecipes(),
+    };
+  },
+  components: {
+    RecipeCard,
+    StepList,
+  },
+  computed: {
+    recipeId() {
+      return this.$route.params.id;
+    },
+    recipe() {
+      return this.recipes.find((recipe) => recipe.id == this.recipeId);
+    },
+  },
+};
+</script>
+
 <template>
-    test recipe view
+  <h1>RECIPE VIEW</h1>
+  <div v-if="recipe">
+    <p>
+      test recipe view. Route params id: {{ $route.params.id }}, Recipe.id is
+      {{ recipe.id }}
+    </p>
+    <RecipeCard
+      :id="recipe"
+      :title="recipe.title"
+      :description="recipe.description"
+      :ingredientCount="recipe.ingredients.length"
+      :cookTimeMinutes="recipe.cookTimeMinutes"
+      :rating="recipe.rating">
+    </RecipeCard>
+
+    <StepList :id="recipe" :steps="recipe.steps"></StepList>
+  </div>
+  <div v-else>
+    <p>Recipe not found</p>
+  </div>
 </template>
