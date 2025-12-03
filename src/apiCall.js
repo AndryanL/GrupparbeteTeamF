801cf4d3090a137fb60ca1986c020fcabca49fa0
vbystroem <veronica.bystroem@gmail.com>
2025-12-03 12:44:47 +0100

    Many small changes, mostly related to styling

--- src/apiCall.js
+++ src/apiCall.js
@@ -84,8 +84,8 @@ import { postRecipesFromJSON } from "./apiFunctions.js";
 
 /* deleteAllRecipes(
   "https://recipes.bocs.se/api/v1/f6a7b8c9-d0e1-4f2a-3b4c-5d6e7f8a9b0c/recipes"
-); */
-
+);
+ */
 postRecipesFromJSON(
   "https://recipes.bocs.se/api/v1/f6a7b8c9-d0e1-4f2a-3b4c-5d6e7f8a9b0c/recipes",
   "./recipes.json"
