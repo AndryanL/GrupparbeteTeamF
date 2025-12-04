a456f23e079b6bf6da92466f7fcaf85011c8db31
mariagileborn-pixel <maria.gileborn@chasacademy.se>
2025-12-04 11:44:44 +0100

    Categoryview css så det matchar homeview

--- views/CategoryView.vue
+++ views/CategoryView.vue
@@ -3,6 +3,7 @@ import { getData } from "../src/apiFunctions.js";
 import RecipeCard from "../src/components/RecipeCard.vue";
 import SearchBar from "../src/components/SearchBar.vue";
 import Category from "@/components/Category.vue";
+import RandomButtonAlt from "@/components/RandomButtonAlt.vue";
 
 export default {
   data() {
@@ -89,29 +90,41 @@ export default {
     RecipeCard,
     SearchBar,
     Category,
+    RandomButtonAlt,
   },
 };
 </script>
 
 <template>
-  <h1 v-if="currentCategory">{{ currentCategory.name }}</h1>
-  <h1 v-else>Alla Kategorier</h1>
-  <SearchBar @search="searchResult" />
-  <div v-if="loading">Loading recipes...</div>
-  <div v-else-if="error" class="error">{{ error }}</div>
-  <div v-else>
-    <div v-if="recipes.length > 0 && filteredRecipes.length === 0" class="no-results">
-      Här var det tomt! Inga recept matchar din sökning.
-    </div>
-    <div v-else>
-      <div v-for="recipe in filteredRecipes" :key="recipe.id" class="container">
-        <RecipeCard class="recipe-card" @click="$router.push({ name: 'recipe', params: { id: recipe.id } })"
-          :recipe="recipe"></RecipeCard>
+  <div class="wrapper">
+    <div class="flex-container">
+      <h1 v-if="currentCategory">{{ currentCategory.name }}</h1>
+      <h1 v-else>Alla Kategorier</h1>
+      <div class="loading" v-if="loading">Loading recipes...</div>
+      <div class="homenav-container">
+        <SearchBar @search="searchResult" />
+        <RandomButtonAlt v-if="recipes.length > 0" class="random-button" :recipes="recipes" />
+      </div>
+      
+      <div class="homebody">
+        <div>
+          <Category class="category" v-if="categories.length > 0" :categories="categories" />
+        </div>
+        <div v-if="loading">Loading recipes...</div>
+        <div v-else-if="error" class="error">{{ error }}</div>
+        <div v-else>
+          <div v-if="recipes.length > 0 && filteredRecipes.length === 0" class="no-results">
+            Här var det tomt! Inga recept matchar din sökning.
+          </div>
+          <div v-else>
+            <div v-for="recipe in filteredRecipes" :key="recipe.id">
+              <RecipeCard class="recipe-card" @click="$router.push({ name: 'recipe', params: { id: recipe.id } })"
+                :recipe="recipe"></RecipeCard>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div v-if="categories.length > 0">
-    <Category :categories="categories" />
   </div>
 </template>
 
@@ -120,6 +133,43 @@ h1 {
   font-family: "Belanosima", sans-serif;
   font-weight: 600;
   margin-left: 0.5rem;
+  text-align: center;
+}
+
+.homebody {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.flex-container {
+  width: min(93.5vw, 768px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.flex-container>* {
+  width: 100%;
+}
+
+.homenav-container {
+  display: flex;
+  height: 3rem;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.random-button {
+  height: 100%;
 }
 
 .recipe-card {
@@ -129,14 +179,28 @@ h1 {
   justify-content: center;
 }
 
+.error {
+  color: red;
+  padding: 1rem;
+}
+
 .no-results {
   font-family: "Playwrite Dk Uloopet", cursive;
   color: var(--color-primary-dark);
   padding: 1rem;
 }
 
-.error {
-  color: red;
+.loading {
+  font-family: "Playwrite Dk Uloopet", cursive;
+  color: var(--color-primary-dark);
   padding: 1rem;
+}
+
+@media screen and (min-width: 992px) {
+  .homebody {
+    margin-left: -16rem;
+    flex-direction: row;
+    align-items: start;
+  }
 }
 </style>
